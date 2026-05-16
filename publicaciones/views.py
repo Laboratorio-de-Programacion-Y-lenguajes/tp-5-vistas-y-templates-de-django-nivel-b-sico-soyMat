0e6d127dce537d9b0ef8183585d5680b104bcd19
soyMat <mazuelamateo16@gmail.com>
2026-05-16 16:36:08 -0300

    agrego vistas para inicio, lista y detalle

--- publicaciones/views.py
+++ publicaciones/views.py
@@ -1,59 +1,27 @@
 from django.views.generic import TemplateView, ListView, DetailView
 from .models import Publicacion
 
-# ---------------------------------------------------------------------------
-# InicioView
-# ---------------------------------------------------------------------------
-# TODO: Implementar InicioView usando TemplateView.
-#
-# Requisitos:
-#   - template_name = "publicaciones/inicio.html"
-#   - Sobreescribir get_context_data() para agregar al contexto:
-#       "titulo"  → str con el nombre del portal
-#       "mensaje" → str de bienvenida
-#
-# Pista:
-#   class InicioView(TemplateView):
-#       template_name = "..."
-#
-#       def get_context_data(self, **kwargs):
-#           context = super().get_context_data(**kwargs)
-#           context["titulo"] = "..."
-#           context["mensaje"] = "..."
-#           return context
+# Vista para la pagina de inicio
+class InicioView(TemplateView):
+    template_name = "publicaciones/inicio.html"
+
+    def get_context_data(self, **kwargs):
+        # Recuperamos el contexto base y le sumamos nuestros datos
+        contexto = super().get_context_data(**kwargs)
+        contexto["titulo"] = "Portal de publicaciones"
+        contexto["mensaje"] = "Bienvenido/a al sitio"
+        return contexto
 
 
-# ---------------------------------------------------------------------------
-# PublicacionListView
-# ---------------------------------------------------------------------------
-# TODO: Implementar PublicacionListView usando ListView.
-#
-# Requisitos:
-#   - model = Publicacion
-#   - context_object_name = "publicacion_list"
-#     (el template accede a esta variable con {% for pub in publicacion_list %})
-#
-# Pista:
-#   class PublicacionListView(ListView):
-#       model = ...
-#       context_object_name = "..."
+# Vista para listar todas las publicaciones
+class PublicacionListView(ListView):
+    model = Publicacion
+    context_object_name = "publicacion_list"
 
 
-# ---------------------------------------------------------------------------
-# PublicacionDetailView
-# ---------------------------------------------------------------------------
-# TODO: Implementar PublicacionDetailView usando DetailView.
-#
-# Requisitos:
-#   - model = Publicacion
-#   - context_object_name = "publicacion"
-#     (el template accede a esta variable con {{ publicacion.titulo }})
-#   - pk_url_kwarg = "publicacion_id"
-#     (indica que el parámetro en la URL se llama "publicacion_id", no "pk")
-#   - Si no existe la publicación → responde automáticamente con 404
-#
-# Pista:
-#   class PublicacionDetailView(DetailView):
-#       model = ...
-#       context_object_name = "..."
-#       pk_url_kwarg = "..."
+# Vista para ver el detalle de una publicacion especifica
+class PublicacionDetailView(DetailView):
+    model = Publicacion
+    context_object_name = "publicacion"
+    # Le decimos a Django como se llama el parametro en la URL
+    pk_url_kwarg = "publicacion_id"
