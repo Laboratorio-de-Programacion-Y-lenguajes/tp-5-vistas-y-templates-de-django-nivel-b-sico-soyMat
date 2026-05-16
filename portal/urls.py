482255e2d4a8cf6f95bb25f4e6b9f81bfff6ce21
soyMat <mazuelamateo16@gmail.com>
2026-05-16 16:27:21 -0300

    rutas app

--- portal/urls.py
+++ portal/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    # TODO: Incluir las rutas de la app publicaciones usando include()
-    # Pista: path("", include("publicaciones.urls")),
+    path('admin/', admin.site.urls),
+    path('', include('publicaciones.urls')),
 ]
